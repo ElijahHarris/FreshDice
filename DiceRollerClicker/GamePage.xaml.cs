3d52fce04f94167112ce41c7e0049f7156bba90a
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2020-07-29 08:29:46 -0600

    button linking

--- DiceRollerClicker/GamePage.xaml.cs
+++ DiceRollerClicker/GamePage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +13,34 @@ namespace DiceRollerClicker
         public GamePage()
         {
             this.InitializeComponent();
+        }
+        public async void BuyGoblin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public async void BuyD4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public async void BuyD6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public async void BuyD8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public async void BuyD10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public async void BuyD12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public async void BuyD20_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
